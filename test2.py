4888373e2b0d0dae2b593b2099cf8cf56915d9d2
Tuannurlaila katamo <tuannurlaila.k@ku.th>
2022-06-07 09:53:47 +0700

    start git

---
+++ test2.py
@@ -0,0 +1,19 @@
+# def area(w,l):
+#     a = w*l
+#     return a
+
+# x = float(input("w :"))
+# y = float(input("l :"))
+
+# area = area(x,y)
+
+# print(area)
+
+
+def area(b,h):
+    a = 0.5*b*h
+    return a
+
+b=float(input("b :"))
+h=float(input("h :"))
+print(area(b,h))
