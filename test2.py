8c40cbb6ca5228e4ec75726ed10d63c679d7a74d
Tuannurlaila katamo <tuannurlaila.k@ku.th>
2022-06-07 12:51:52 +0700

    start git

--- test2.py
+++ test2.py
@@ -10,10 +10,11 @@
 # print(area)
 
 
-def area(b,h):
+def area(b, h):
     a = 0.5*b*h
     return a
 
-b=float(input("b :"))
-h=float(input("h :"))
-print(area(b,h))
+
+b = float(input("b :"))
+h = float(input("h :"))
+print(area(b, h))
